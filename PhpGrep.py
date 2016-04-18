9890f8178142cb8a49aefc5b1c9b9984d6075e58
Deschavanne Arnaud <deschavannea@pc167.magellan.local>
2016-04-18 10:30:43 +0200

    hardcode some value

--- PhpGrep.py
+++ PhpGrep.py
@@ -5,8 +5,13 @@
 extension_file = ['php','php3','php4','ini','include','php.ini','old','bak']
 
 ext = ','.join(extension_file)
-
+ 
 dangerous_func = open ('dangerous_functions.txt','r')
+print " fgrep -Ri --color --include=\*.{php,php3,php4,ini,include,php.ini,old,bak} ' $_GET(' ./"
+print " fgrep -Ri --color --include=\*.{php,php3,php4,ini,include,php.ini,old,bak} ' $_GET[' ./"
+print " fgrep -Ri --color --include=\*.{php,php3,php4,ini,include,php.ini,old,bak} ' $_POST(' ./"
+print " fgrep Ri --color --include=\*.{php,php3,php4,ini,include,php.ini,old,bak} ' $_POST[' ./"
+print " fgrep Ri --color --include=\*.{php,php3,php4,ini,include,php.ini,old,bak} ' `' ./"
 for i in dangerous_func:
         i=i.replace("\n", "")
         print ' fgrep -Ri --color --include=\*.{'+ext+'} '+'\' '+i+'(\''+' ./'
